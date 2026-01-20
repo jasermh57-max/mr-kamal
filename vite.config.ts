4dbc5da9a82fd43456357066181340d64a2736dd
jasermh57-max <jasermh57@gmail.com>
2026-01-20 19:53:24 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: './', // <--- أضف هذا السطر هنا بالضبط (ضروري جداً للأندرويد)
       server: {
         port: 3000,
         host: '0.0.0.0',
